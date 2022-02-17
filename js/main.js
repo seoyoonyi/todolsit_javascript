31fd9bdd85df2d702cec62ea588c352cb63c674a
seoyoonyi <yiseoyoon@gmail.com>
2022-02-18 00:49:57 +0900

    1차 코드정리 완료

--- js/main.js
+++ js/main.js
@@ -5,42 +5,53 @@ const incompleteTaskHolder = document.getElementById('incomplete-tasks'); // 일
 const completedTasksHolder = document.getElementById('completed-tasks'); // 일 완료
 
 /* 함수 */
-// 새로운 일 생성
-const createNewTaskElement = (taskString) => {
-  const listItem = document.createElement('li'); // li item 생성
-  const checkBox = document.createElement('input'); // Input (checkbox)
-  const label = document.createElement('label'); // Label
-  const editInput = document.createElement('input'); // Input(text)
-  const editButton = document.createElement('button'); // Button.edit
-  const deleteButton = document.createElement('button'); // Button.delete
+// 새로운 일과 이벤트 추가요소 생성
+const newTaskGroup = {
+  createElements: function (taskString) {
+    const listItem = document.createElement('li'); // li item 생성
+    const checkBox = document.createElement('input'); // Input (checkbox)
+    const label = document.createElement('label'); // Label
+    const editInput = document.createElement('input'); // Input(text)
+    const editButton = document.createElement('button'); // Button.edit
+    const deleteButton = document.createElement('button'); // Button.delete
 
-  checkBox.type = 'checkbox'; //element요소의 프로퍼티
-  editInput.type = 'text';
-  editButton.innerText = 'Edit';
-  editButton.className = 'edit';
-  deleteButton.innerText = 'Delete';
-  deleteButton.className = 'delete';
-  label.innerText = taskString;
+    checkBox.type = 'checkbox'; //element요소의 프로퍼티
+    editInput.type = 'text';
+    editButton.innerText = 'Edit';
+    editButton.className = 'edit';
+    deleteButton.innerText = 'Delete';
+    deleteButton.className = 'delete';
+    label.innerText = taskString;
 
-  listItem.appendChild(checkBox); //element요소 추가
-  listItem.appendChild(label);
-  listItem.appendChild(editInput);
-  listItem.appendChild(editButton);
-  listItem.appendChild(deleteButton);
+    listItem.appendChild(checkBox); //element요소 추가
+    listItem.appendChild(label);
+    listItem.appendChild(editInput);
+    listItem.appendChild(editButton);
+    listItem.appendChild(deleteButton);
 
-  return listItem;
+    return listItem;
+  },
+  createEvents: function (taskListItem, checkBoxEventHandler) {
+    const editButton = taskListItem.querySelector('button.edit');
+    const deleteButton = taskListItem.querySelector('button.delete');
+    const checkBox = taskListItem.querySelector('input[type=checkbox]');
+
+    editButton.onclick = editTask;
+    deleteButton.onclick = deleteTask;
+    checkBox.onchange = checkBoxEventHandler;
+  },
 };
 
-//일 추가 생성
-const addTask = function () {
+// 추가했을 시
+const addTask = () => {
   const listItemName = taskInput.value || 'New Item';
-  const listItem = createNewTaskElement(listItemName);
+  const listItem = newTaskGroup.createElements(listItemName);
   incompleteTaskHolder.appendChild(listItem);
-  bindTaskEvents(listItem, taskCompleted);
+  newTaskGroup.createEvents(listItem, completeGroup.taskCompleted);
   taskInput.value = '';
 };
 
-//일 편집
+// 편집할 시
 const editTask = (e) => {
   const listItem = e.target.parentNode;
   const editInput = listItem.querySelector('input[type=text]');
@@ -58,44 +69,40 @@ const editTask = (e) => {
   listItem.classList.toggle('editMode');
 };
 
-//일 삭제
+// 완료 여부 체크시
+const completeGroup = {
+  taskCompleted: function (e) {
+    const listItem = e.target.parentNode;
+    completedTasksHolder.appendChild(listItem);
+    newTaskGroup.createEvents(listItem, completeGroup.taskIncomplete);
+  },
+  taskIncomplete: function (e) {
+    const listItem = e.target.parentNode;
+    incompleteTaskHolder.appendChild(listItem);
+    newTaskGroup.createEvents(listItem, completeGroup.taskCompleted);
+  },
+};
+
+// 삭제
 const deleteTask = (e) => {
   const listItem = e.target.parentNode;
   const ul = listItem.parentNode;
   ul.removeChild(listItem);
 };
 
-//일 완료된 상태
-const taskCompleted = (e) => {
-  const listItem = e.target.parentNode;
-  completedTasksHolder.appendChild(listItem);
-  bindTaskEvents(listItem, taskIncomplete);
-};
-
-//일 완료
-const taskIncomplete = (e) => {
-  const listItem = e.target.parentNode;
-  incompleteTaskHolder.appendChild(listItem);
-  bindTaskEvents(listItem, taskCompleted);
-};
-
-/* 이벤트 */
-const bindTaskEvents = function (taskListItem, checkBoxEventHandler) {
-  const editButton = taskListItem.querySelector('button.edit');
-  const deleteButton = taskListItem.querySelector('button.delete');
-  const checkBox = taskListItem.querySelector('input[type=checkbox]');
-
-  editButton.onclick = editTask;
-  deleteButton.onclick = deleteTask;
-  checkBox.onchange = checkBoxEventHandler;
-};
-
+/* 이벤트  */
 addButton.addEventListener('click', addTask);
 
 for (let i = 0; i < incompleteTaskHolder.children.length; i++) {
-  bindTaskEvents(incompleteTaskHolder.children[i], taskCompleted);
+  newTaskGroup.createEvents(
+    incompleteTaskHolder.children[i],
+    completeGroup.taskCompleted
+  );
 }
 
 for (let i = 0; i < completedTasksHolder.children.length; i++) {
-  bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
+  newTaskGroup.createEvents(
+    completedTasksHolder.children[i],
+    completeGroup.taskIncomplete
+  );
 }
